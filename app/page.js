cb7c3876e7909e94190e82b210b0f0135a721f60
Olawale Ogundeji <abiodungndj@gmail.com>
2024-09-07 23:59:50 +0100

    Add footer

--- app/page.js
+++ app/page.js
@@ -1,20 +1,17 @@
-'use client';
+"use client"
+
 import { useEffect, useState } from 'react';
 import { SignedIn, SignedOut, UserButton, useUser } from '@clerk/nextjs';
-// import { useUser } from '@clerk/clerk-react'
 import { AppBar, Box, Button, Container, Grid, Toolbar, Typography } from '@mui/material';
 import Head from 'next/head';
 import getStripe from '@/utils/get-stripe';
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const router = useRouter();
   const { isLoaded, user } = useUser();
-  console.log('isLoaded:', isLoaded); // should be true or false
-console.log('user:', user); // should be user object or undefined
   const [typedTexts, setTypedTexts] = useState(['', '', '']); // Initialize empty typed texts for each box
 
-  // Original texts to be animated
   const texts = [
     'Suggests exercises tailored to your fitness goals, experience level, and available equipment.',
     'Keeps you motivated with personalised tips to maintain a consistent workout routine.',
@@ -22,24 +19,23 @@ console.log('user:', user); // should be user object or undefined
   ];
 
   useEffect(() => {
-    // Animate each text separately
     texts.forEach((text, index) => {
       let currentIndex = 0;
       const interval = setInterval(() => {
         if (currentIndex < text.length) {
           setTypedTexts((prev) => {
             const newTypedTexts = [...prev];
-            newTypedTexts[index] = text.slice(0, currentIndex + 1); // Correctly slice text up to current character
+            newTypedTexts[index] = text.slice(0, currentIndex + 1);
             return newTypedTexts;
           });
           currentIndex++;
         } else {
-          clearInterval(interval); // Clear the interval after the text is fully typed
+          clearInterval(interval);
         }
-      }, 100); // Adjust typing speed here
-      return () => clearInterval(interval); // Cleanup interval on component unmount
+      }, 100);
+      return () => clearInterval(interval);
     });
-  }, []); // Empty dependency array to ensure this runs once on component mount
+  }, []);
 
   const handleClick = () => {
     if (!user) {
@@ -75,161 +71,172 @@ console.log('user:', user); // should be user object or undefined
 
   return (
     <Box
-    sx={{
-      minHeight: "100vh", // Ensures full viewport height
-      width: "100vw",     // Ensures full viewport width
-      backgroundColor: 'lightcyan', // Your desired background color
-    }}
+      sx={{
+        minHeight: "100vh",
+        width: "100vw",
+        backgroundColor: 'lightcyan',
+      }}
     >
-    <Container maxWidth="100vw">
-      <Head>
-        <title>Fitness Companion</title>
-        <meta
-          name="description"
-          content="Chat with Fitness Companion, an AI that understands your goals and helps you stay fit, focused, and consistent."
-        />
-      </Head>
+      <Container maxWidth="100vw">
+        <Head>
+          <title>Fitness Companion</title>
+          <meta
+            name="description"
+            content="Chat with Fitness Companion, an AI that understands your goals and helps you stay fit, focused, and consistent."
+          />
+        </Head>
 
-      <AppBar position="sticky" color="secondary">
-        <Toolbar>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            Fitness Companion
+        <AppBar position="sticky" color="secondary">
+          <Toolbar>
+            <Typography variant="h6" style={{ flexGrow: 1 }}>
+              Fitness Companion
+            </Typography>
+            <SignedOut>
+              <Button color="inherit" href="/sign-in">
+                Login
+              </Button>
+              <Button color="inherit" href="/sign-up">
+                Sign up
+              </Button>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Toolbar>
+        </AppBar>
+
+        <Box sx={{ textAlign: 'center', my: 4 }}>
+          <Typography variant="h3" gutterBottom>
+            Welcome to Your Fitness Companion!
           </Typography>
-          {/* isLoaded: {isLoaded} user: {user} */}
-          <SignedOut>
-            <Button color="inherit" href="/sign-in">
-              Login
-            </Button>
-            <Button color="inherit" href="/sign-up">
-              Sign up
-            </Button>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </Toolbar>
-      </AppBar>
+          <Typography variant="h5" gutterBottom>
+            A conversational AI that understands your fitness goals and helps you select the perfect exercises to stay fit, focused, and consistent!
+          </Typography>
+          <Button onClick={handleClick} variant="contained" color="secondary" sx={{ mt: 2 }}>
+            Get Started
+          </Button>
+        </Box>
 
-      <Box sx={{ textAlign: 'center', my: 4 }}>
-        <Typography variant="h3" gutterBottom>
-          Welcome to Your Fitness Companion!
-        </Typography>
-        <Typography variant="h5" gutterBottom>
-          A conversational AI that understands your fitness goals and helps you select the perfect exercises to stay fit, focused, and consistent!
-        </Typography>
-        <Button onClick={handleClick} variant="contained" color="secondary" sx={{ mt: 2 }}>
-          Get Started
-        </Button>
-      </Box>
+        <Box sx={{ my: 6, textAlign: 'center' }}>
+          <Typography
+            variant="h4"
+            sx={{
+              fontWeight: 'bold',
+              fontSize: '2.5rem',
+              color: 'primary.main',
+              mb: 4,
+            }}
+          >
+            What Your Fitness Companion Offers!
+          </Typography>
 
-      <Box sx={{ my: 6, textAlign: 'center' }}>
-        <Typography
-          variant="h4"
-          sx={{
-            fontWeight: 'bold',
-            fontSize: '2.5rem',
-            color: 'primary.main',
-            mb: 4,
-          }}
-        >
-          What Your Fitness Companion Offers!
-        </Typography>
+          <Grid container spacing={4} justifyContent="center" alignItems="center" sx={{ textAlign: 'center' }}>
+            {[
+              {
+                title: 'Personalised Exercise Recommendations:',
+                text: texts[0],
+              },
+              {
+                title: 'Motivation and Consistency Support:',
+                text: texts[1],
+              },
+              {
+                title: 'Alternative Exercise Options:',
+                text: texts[2],
+              },
+            ].map((item, index) => (
+              <Grid item xs={12} md={5} key={index}>
+                <Box
+                  sx={{
+                    p: 3,
+                    border: '2px solid',
+                    borderRadius: 2,
+                    borderColor: 'success.main',
+                    height: '200px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center',
+                    overflow: 'hidden',
+                    wordWrap: 'break-word',
+                  }}
+                >
+                  <Typography variant="h6" sx={{ mb: 2, whiteSpace: 'nowrap', fontWeight: 'bold' }}>
+                    {item.title}
+                  </Typography>
+                  <Typography
+                    component="span"
+                    sx={{
+                      animation: 'typing 3s steps(40, end), blink-caret .75s step-end infinite',
+                      overflow: 'hidden',
+                      borderRight: '3px solid',
+                      display: 'inline-block',
+                      fontWeight: 'bold',
+                      color: '#007BFF',
+                    }}
+                  >
+                    {typedTexts[index]}
+                  </Typography>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
 
-        {/* Grid Container */}
-        <Grid container spacing={4} justifyContent="center" alignItems="center" sx={{ textAlign: 'center' }}>
-          {/* Grid Items */}
-          {[
-            {
-              title: 'Personalised Exercise Recommendations:',
-              text: texts[0], // Original text
-            },
-            {
-              title: 'Motivation and Consistency Support:',
-              text: texts[1], // Original text
-            },
-            {
-              title: 'Alternative Exercise Options:',
-              text: texts[2], // Original text
-            },
-          ].map((item, index) => (
-            <Grid item xs={12} md={4} key={index}>
+        <Box sx={{ my: 6, textAlign: "center" }}>
+          <Typography variant="h4" gutterBottom>
+            Subscription
+          </Typography>
+          <Grid container spacing={4} justifyContent="center" alignItems="center" sx={{ textAlign: 'center' }}>
+            <Grid item xs={12} md={4}>
               <Box
                 sx={{
-                  p: 4,
-                  border: '2px solid',
+                  p: 2,
+                  border: "2px solid",
                   borderRadius: 2,
-                  borderColor: 'success.main',
-                  height: '200px',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  textAlign: 'center',
-                  overflow: 'hidden',
-                  wordWrap: 'break-word',
+                  borderColor: "secondary.main",
                 }}
               >
-                {/* Centered title text */}
-                <Typography variant="h6" sx={{ mb: 2, whiteSpace: 'nowrap', fontWeight: 'bold' }}>
-                  {item.title}
+                <Typography variant="h5" gutterBottom sx={{ fontWeight: 'bold' }}>
+                  Be a Pro
                 </Typography>
-                {/* Animated Text */}
-                <Typography 
-                component="span"
-                sx={{
-                  animation: 'typing 3s steps(40, end), blink-caret .75s step-end infinite',
-                  overflow: 'hidden',
-                  borderRight: '3px solid',
-                  display: 'inline-block',
-                  fontWeight: 'bold',
-                  color: '#007BFF', // Change to your preferred color here
-                }}
+                <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold' }}>
+                  $5 / month
+                </Typography>
+                <Typography color='#007BFF'>
+                  {" "}
+                  Subscribe to Fitness Companion to stay fit and consistent.
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  sx={{ mt: 2 }}
+                  onClick={handleSubmit}
                 >
-                  {typedTexts[index]}
-                </Typography>
+                  Choose Pro
+                </Button>
               </Box>
             </Grid>
-          ))}
-        </Grid>
-      </Box>
-
-      <Box sx={{ my: 6, textAlign: "center" }}>
-        <Typography variant="h4" gutterBottom>
-          Subscription
-        </Typography>
-        <Grid container spacing={4} justifyContent="center" alignItems="center" sx={{ textAlign: 'center' }}>
-          <Grid item xs={12} md={4}>
-            <Box
-              sx={{
-                p: 4,
-                border: "2px solid",
-                borderRadius: 2,
-                borderColor: "secondary.main",
-              }}
-            >
-              <Typography variant="h5" gutterBottom sx={{fontWeight: 'bold'}}>
-                Be a Pro
-              </Typography>
-              <Typography variant="h6" gutterBottom sx={{fontWeight: 'bold'}}>
-                 $5 / month
-              </Typography>
-              <Typography color='#007BFF'>
-                {" "}
-                Subscribe to Fitness Companion to stay fit and consistent.
-              </Typography>
-              <Button 
-                variant="contained" 
-                color="secondary" 
-                sx={{mt: 2}} 
-                onClick={handleSubmit}
-              >
-                Choose Pro
-              </Button>
-            </Box>
           </Grid>
-        </Grid>
-      </Box>
-    </Container>
+        </Box>
+
+        {/* Footer */}
+        <Box
+          sx={{
+            backgroundColor: 'lightgrey',
+            padding: '1rem',
+            textAlign: 'center',
+            position: 'fixed',
+            bottom: 0,
+            width: '100%',
+          }}
+        >
+          <Typography variant="body2" color="textSecondary">
+            © 2024 Wale Ogundeji. All rights reserved.
+          </Typography>
+        </Box>
+      </Container>
     </Box>
   );
 }
