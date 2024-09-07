35b130b331604ed19ca9d47e9112686f245ae4f2
Olawale Ogundeji <abiodungndj@gmail.com>
2024-09-07 23:23:17 +0100

    configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      // Only modify Webpack configuration for server-side
+      if (!isServer) {
+        // Provide polyfills or fallbacks for Node.js modules in client-side
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          buffer: false, // Disable resolving buffer on the client-side
+        };
+      }
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
